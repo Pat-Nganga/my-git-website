f85e397c2f8902d7119a62e6b94f6140f8716245
Patricia <wambuipatricia3@gmail.com>
2023-03-12 17:34:13 +0300

    Added Event Listener & comment

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("link-about")
 .addEventListener("click", fnClick)
 
+// function to be executed when the About button is clicked
 function fnClick(event) {
     alert("You have clicked the About button")
 }
